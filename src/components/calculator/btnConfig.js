13f5742e2b9dd4df211cac9fa71a2b17e2292f20
rzzel <ruzzdabid@gmail.com>
2023-11-08 22:09:21 +0800

    hehe

---
+++ src/components/calculator/btnConfig.js
@@ -0,0 +1,108 @@
+export const BTN_ACTIONS = {
+  ADD: "ADD",
+  CALC: "CALC",
+  DELETE: "DELETE",
+};
+
+export const btns = [
+  {
+    display: "C",
+    action: BTN_ACTIONS.DELETE,
+    class: "btn__op",
+  },
+  {
+    display: "(",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: ")",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "/",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "7",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "8",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "9",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "x",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "4",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "5",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "6",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "-",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "1",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "2",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "3",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "+",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "00",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "0",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: ".",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__num",
+  },
+  {
+    display: "=",
+    action: BTN_ACTIONS.CALC,
+    class: "btn__op",
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sx: "330px",
+      // => @media (min-width: 330px) { ... }
+
+      xs: "360px",
+      // => @media (min-width: 360px) { ... }
+
+      ms: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontFamily: {
+      montserrat: ["Montserrat Alternates", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "body-bg": "#64748b",
+        "txt-color": "#09090b",
+        "txt-white": "#fecaca",
+        "calc-res-color": "#dc2626",
+        "calc-btn-color": "#eab308",
+        
+        "calc-bg": "#7dd3fc",
+      },
+    },
+  },
+  plugins: [],
+};
